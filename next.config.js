edf962834f059b102a1aec1dfb18d8d5374ecfd4
igventurelli <igventurelli@gmail.com>
2024-05-31 14:07:34 -0300

    chore(deployment): adding deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+  };
 
 module.exports = withContentlayer(nextConfig)
