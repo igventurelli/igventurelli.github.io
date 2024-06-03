734d22613da690b51803d9c8b74c0a84adef5dc9
igventurelli <igventurelli@gmail.com>
2024-06-03 12:24:54 -0300

    chore(about): wip

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ import WidgetNewsletter from '@/components/widget-newsletter'
 import WidgetSponsor from '@/components/widget-sponsor'
 import AboutImg from '@/public/images/about.png'
 import Experience from '@/components/experience'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'About - Igor Venturelli',
@@ -19,29 +20,26 @@ export default function About() {
 
           <section>
             { /* Page title */}
-            <h1 className="h1 font-aspekta mb-5">Hi. I'm Mark <span className="inline-flex relative text-sky-500 before:absolute before:inset-0 before:bg-sky-200 dark:before:bg-sky-500 before:opacity-30 before:-z-10 before:-rotate-2 before:translate-y-1/4">@mrk27</span> Ivings 🤟</h1>
-            <Image className="w-full" src={AboutImg} width={692} height={390} alt="About" />
+            <h1 className="h1 font-aspekta mb-5">Hi. I'm Igor <span className="inline-flex relative text-sky-500 before:absolute before:inset-0 before:bg-sky-200 dark:before:bg-sky-500 before:opacity-30 before:-z-10 before:-rotate-2 before:translate-y-1/4"><Link href="https://links.igventurelli.io" target="_blank">@igventurelli</Link></span> Venturelli ✌🏻</h1>
+            <Image className="w-full mt-5 mb-5" src={AboutImg} width={692} height={390} alt="About" />
             { /* Page content */}
             <div className="text-slate-500 dark:text-slate-400 space-y-8">
               <div className="space-y-4">
                 <h2 className="h3 font-aspekta text-slate-800 dark:text-slate-100">Short Bio</h2>
                 <p>
-                  I'm a software engineer with more than 10 years of experience in a variety of domains. For the past few years, I've focused on highload server-side projects, distributed systems, and platform development - tinkering with infrastructure, all things containers and Cloud Native.
+                  I'm a software engineer with 15 years of experience in a variety of domains. For the past few years, I've focused on highload server-side projects, distributed systems, and platform development - tinkering with infrastructure, all things containers and Cloud Native.
                 </p>
                 <p>
-                  While there isn't a Wikipedia page about me (sorry folks!), a media bio is available below.
+                  I know the picture above is pretty strange for this context, but you know what? This is who I am, I love CrossFit and technology, so this is me (in essence).
                 </p>
               </div>
               <div className="space-y-4">
                 <h2 className="h3 font-aspekta text-slate-800 dark:text-slate-100">Career</h2>
                 <p>
-                  In my role as a Senior Software Engineer for Google Chrome, I am responsible for developing and maintaining the <a className="font-medium text-sky-500 hover:underline" href="#0">Chrome Web Browser</a>.
+                  In my role as a Senior Software Engineer for <a className="font-medium text-sky-500 hover:underline" href="https://www.truelogic.io/">Truelogic</a>, I am responsible for developing and maintaining the <a className="font-medium text-sky-500 hover:underline" href="https://www.rakuten.com/">Rakuten Rewards</a>.
                 </p>
                 <p>
-                  My work involves developing and testing new features, optimizing performance and security, and ensuring the browser works for users around the world. I also work closely with other Google teams ensure Chrome is well-integrated with other <a className="font-medium text-sky-500 hover:underline" href="#0">Google</a> products and services.
-                </p>
-                <p>
-                  As CTO of AppForYou, <strong className="font-medium text-slate-800 dark:text-slate-100">I am responsible for leading</strong> the technical teamand developing the company's technology strategy. I work closely with the engineering team to ensure that the products and services we provide are secure.
+                  My work involves developing and testing new features, optimizing performance and security, and ensuring the cashback system works for users around the world.
                 </p>
               </div>
               
@@ -50,7 +48,7 @@ export default function About() {
               <div className="space-y-4">
                 <h2 className="h3 font-aspekta text-slate-800 dark:text-slate-100">Let's Connect</h2>
                 <p>
-                  I'm excited to connect with others via <a className="font-medium text-sky-500 hover:underline" href="#0">email</a> and <a className="font-medium text-sky-500 hover:underline" href="#0">Twitter</a> to chat about projects and ideas. Currently, I'm not taking on freelance projects, but I am open to hearing about potential opportunities, discussing them with you and then potentially collaborating if it's a good fit.
+                  I'm excited to connect with others via <a className="font-medium text-sky-500 hover:underline" href="mailto:igventurelli@gmail.com">email</a> and <a className="font-medium text-sky-500 hover:underline" href="https://www.linkedin.com/in/igventurelli/">LinkedIn</a> to chat about projects and ideas. I am open to hearing about potential opportunities, discussing them with you and then potentially collaborating if it's a good fit.
                 </p>
               </div>
             </div>
