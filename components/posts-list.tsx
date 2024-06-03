f5e1d96be82216e9662996fa6f5790cd51aac182
igventurelli <igventurelli@gmail.com>
2024-06-03 13:29:02 -0300

    chore(index): fix

--- components/posts-list.tsx
+++ components/posts-list.tsx
@@ -54,7 +54,7 @@ export default function PostsList() {
 
     if (loading) {
         return (
-            <div role="status" className="max-w-sm animate-pulse">
+            <div role="status" className="max-w-sm animate-pulse mt-5">
                 <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
                 <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
                 <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
